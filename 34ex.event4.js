06fd2c73160d2b1bc1fdd587b0d09403c6ffce49
hyewon046 <gpdjsl1216@gmail.com>
2025-01-17 17:50:23 +0900

    406

--- 34ex.event4.js
+++ 34ex.event4.js
@@ -1,3 +1,10 @@
 // event실습 4 : 성적처리 (동적 테이블 생성)
 // 데이터 : 성명,국어,영어,수학
 // 기능 : 등록, 삭제, 개인총점, 과목총점 연산
+
+const regist = document.querySelector('button');
+regist.addEventListener('click', e=>{
+    const type = document.querySelectorAll('input[type="text"]');
+
+    //appendchild도 써야할것같으다
+});
